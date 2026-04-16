998fe6238e9aef80f11bc3e408f21d724410bcbd
Aleanny Burgos <aleannyburgos@gmail.com>
2026-04-16 10:45:48 -0400

    troubleshoot connection

---
+++ fir_filter_tb.vhd
@@ -0,0 +1,43 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity fir_filter_tb is
+end fir_filter_tb;
+
+architecture tb of fir_filter_tb is
+
+    signal clock   : std_logic := '0';
+    signal clear : std_logic := '0';
+    signal y     : std_logic_vector(15 downto 0);
+
+begin
+
+    uut: entity work.fir_filter
+        port map(
+            clock   => clock,
+            clear => clear,
+            y     => y
+        );
+
+    -- 10 ns clock period
+    clk_process : process
+    begin
+        while true loop
+            clock <= '0';
+            wait for 5 ns;
+            clock <= '1';
+            wait for 5 ns;
+        end loop;
+    end process;
+
+    stim_process : process
+    begin
+        clear <= '1';
+        wait for 20 ns;
+        clear <= '0';
+
+        wait for 300 ns;
+
+        wait;
+    end process;
+end tb;
